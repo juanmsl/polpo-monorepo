dc534efda8ee4b76190b16eb73532f9cf739b61b
Juan Sánchez <juanmsl.pk@gmail.com>
2026-05-22 02:14:14 -0500

    fix build process

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -1,15 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     domains: [],
-  }
+  },
 };
 
 export default nextConfig;
