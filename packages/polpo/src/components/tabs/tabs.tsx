12de0b8ba8b8c70bd7a7dc13d5b64925c080428e
Juan Sánchez <juanmsl.pk@gmail.com>
2026-08-07 16:26:46 -0500

    making crdit page responsive

--- packages/polpo/src/components/tabs/tabs.tsx
+++ packages/polpo/src/components/tabs/tabs.tsx
@@ -1,27 +1,43 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useCallback, useContext, useEffect, useState } from 'react';
 
 import { cn } from '../../helpers';
 
 import { TabsList as TabListComponent, TabListProps } from './tabs-list';
 import './tabs.styles.css';
 
+type TabStatus = {
+  isHidden: boolean;
+};
+
 type TabsContextState = {
   openTab: string;
   changeOpenTab: (id: string) => void;
+  tabsStatus: Record<string, TabStatus>;
+  changeTabStatus: (id: string, status: TabStatus) => void;
 };
 
 const TabsContext = createContext<TabsContextState | null>(null);
 
-const useTab = (id: string): [boolean, () => void] => {
+const useTab = (id: string, isHidden = false) => {
   const context = useContext(TabsContext);
 
   if (!context) {
     throw new Error('You cant use this component out off an Tabs component');
   }
 
-  const { openTab, changeOpenTab } = context;
+  const { openTab, changeOpenTab, tabsStatus, changeTabStatus } = context;
 
-  return [openTab === id, () => changeOpenTab(id)];
+  useEffect(() => {
+    if (isHidden !== undefined) {
+      changeTabStatus(id, { isHidden });
+    }
+  }, [id, isHidden, changeTabStatus]);
+
+  return {
+    isOpen: openTab === id,
+    openTab: () => changeOpenTab(id),
+    isHidden: tabsStatus[id]?.isHidden,
+  };
 };
 
 const useTabsContext = (): TabsContextState => {
@@ -42,6 +58,7 @@ type TabsProps = {
 
 export const Tabs = ({ children, defaultOpenTab, onChange }: TabsProps) => {
   const [openTab, setOpenTab] = useState(defaultOpenTab);
+  const [tabs, setTabs] = useState<TabsContextState['tabsStatus']>({});
 
   useEffect(() => {
     if (onChange) {
@@ -49,11 +66,26 @@ export const Tabs = ({ children, defaultOpenTab, onChange }: TabsProps) => {
     }
   }, [openTab, onChange]);
 
+  const changeOpenTab = useCallback<TabsContextState['changeOpenTab']>(id => setOpenTab(id), []);
+
+  const changeTabStatus = useCallback<TabsContextState['changeTabStatus']>(
+    (id, status) => {
+      setTabs(prev => ({ ...prev, [id]: status }));
+
+      if (openTab === id && status.isHidden) {
+        changeOpenTab(defaultOpenTab);
+      }
+    },
+    [changeOpenTab, defaultOpenTab, openTab],
+  );
+
   return (
     <TabsContext.Provider
       value={{
         openTab,
-        changeOpenTab: id => setOpenTab(id),
+        changeOpenTab,
+        changeTabStatus,
+        tabsStatus: tabs,
       }}
     >
       {children}
@@ -61,17 +93,18 @@ export const Tabs = ({ children, defaultOpenTab, onChange }: TabsProps) => {
   );
 };
 
-type TabProps = {
+export type TabProps = {
   id: string;
   children: React.ReactNode;
   className?: string | ((isActive: boolean) => string);
+  isHidden?: boolean;
   style?: React.CSSProperties;
   onClick?: (e: React.MouseEvent<HTMLSpanElement>) => void;
   ref?: React.RefObject<HTMLSpanElement | null>;
 };
 
-const Tab = ({ id, children, className = '', style = {}, onClick, ref }: TabProps) => {
-  const [isOpen, openTab] = useTab(id);
+const Tab = ({ id, children, isHidden = false, className = '', style = {}, onClick, ref }: TabProps) => {
+  const { isOpen, openTab } = useTab(id, isHidden);
 
   const handleClick = (e: React.MouseEvent<HTMLSpanElement>) => {
     openTab();
@@ -80,6 +113,10 @@ const Tab = ({ id, children, className = '', style = {}, onClick, ref }: TabProp
       onClick(e);
     }
   };
+
+  if (isHidden) {
+    return null;
+  }
 
   return (
     <span
@@ -99,9 +136,9 @@ type TabPanelProps = {
 };
 
 const TabPanel = ({ id, children }: TabPanelProps) => {
-  const [isOpen] = useTab(id);
+  const { isOpen, isHidden } = useTab(id);
 
-  return isOpen ? children : null;
+  return isOpen && !isHidden ? children : null;
 };
 
 const TabList = ({ children, ...props }: Omit<TabListProps, 'openTab'>) => {
